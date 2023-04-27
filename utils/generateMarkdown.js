90dfe2bed051d49f9d9895019d53a2feb2e90b29
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-27 00:00:22 -0500

    working on building out renderBadge cases

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,43 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 // USE SWITCH STATEMENT TO FIND A MATCH FOR THE USER'S SELECTED BADGE - I ASKED CHAT GPT TO GIVE ME A DEFINITION: "The switch statement is often used as a more concise alternative to a long chain of if-else statements, especially when you need to compare the value of a single variable against multiple possible values."
+// https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
+// https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>
 
 function renderLicenseBadge(license) {
   let badge;
 
   switch (license) {
     case 'MIT':
-      badge = { name: '', color: ''};
+      badge = { name: 'MIT', color: 'yellow' };
       break;
     case 'Apache':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'GPLv2':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'GPLv3':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'LGPLv3':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'AGPLv3':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'Mozilla':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'Unlicense':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'Eclipse':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'IBM':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
     case 'ISC':
-      badge = { name: '', color: ''};
+      badge = { name: '', color: '' };
       break;
 // DEFAULT NULL LICENSE VALUE
     default: 'other/none'
