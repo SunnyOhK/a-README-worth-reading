4cea265157f6e3339cbffe307240dba25484389e
SunnyOhK <127900916+SunnyOhK@users.noreply.github.com>
2023-04-29 20:02:01 -0500

    I got it to woooork! renders license section, badge, and table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,59 +4,75 @@
 // https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
 // https://img.shields.io/static/v1?label=<LABEL>&message=<MESSAGE>&color=<COLOR>
 // '', '', '', '', '', '', '', '', 'other/none'
-function renderBadge(data) {
+function renderLicenses(license) {
   let badge;
+  let section;
 
-  switch (data.license) {
+  switch (license) {
     case 'MIT':
       badge = '[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://choosealicense.com/licenses/mit)';
+      section = '## License\nThis project is licensed under: [MIT](https://choosealicense.com/licenses/mit/)';
       break;
     case 'Apache 2.0':
       badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://choosealicense.com/licenses/apache-2.0)';
+      section = '## License\nThis project is licensed under: [Apache 2.0](https://choosealicense.com/licenses/apache-2.0)'
       break;
     case 'GNU GPLv3':
       badge = '[![License](https://img.shields.io/badge/License-GPL_v3-blue.svg)](https://choosealicense.com/licenses/gpl-3.0)';
+      section = '## License\nThis project is licensed under: [GPL v3](https://choosealicense.com/licenses/gpl-3.0)'
       break;
     case 'GNU AGPLv3':
       badge = '[![License](https://img.shields.io/badge/License-AGPL_v3-red.svg)](https://choosealicense.com/licenses/agpl-3.0)';
+      section = '## License\nThis project is licensed under: [AGPL v3](https://choosealicense.com/licenses/agpl-3.0)'
       break;
     case 'GNU LGPLv3':
       badge = '[![License](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://choosealicense.com/licenses/lgpl-3.0)';
+      section = '## License\nThis project is licensed under: [LGPL v3](https://choosealicense.com/licenses/lgpl-3.0)'
       break;
     case 'Mozilla 2.0':
       badge = '[![License](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://choosealicense.com/licenses/mpl-2.0)';
+      section = '## License\nThis project is licensed under: [MPL 2.0](https://choosealicense.com/licenses/mpl-2.0)'
       break;
     case 'The Unlicense':
       badge = '[![License](https://img.shields.io/badge/License-Unlicense-blue.svg)](https://choosealicense.com/licenses/unlicense)';
+      section = '## License\nThis project is licensed under: [Unlicense](https://choosealicense.com/licenses/unlicense)';
+
       break;
     case 'Boost Software':
       badge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://choosealicense.com/licenses/bsl-1.0)';
+      section = '## License\nThis project is licensed under: [Boost Software](https://choosealicense.com/licenses/bsl-1.0/)';
+
       break;
     default:
       badge = '';
+      section = '';
   }
-  return badge;
+  return {
+    badge: badge,
+    section: section
+  }
 }
-
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseMD() {
-  if (license) {
-    licenseMd = 'This product is licensed under:'
-  } else {
-    licenseMd = ''
-  }
+// function renderLicenseMD(license) {
+//   if (license) {
+//     licenseMd = 'This product is licensed under:'
+//   } else {
+//     licenseMd = ''
+//   }
 
-  return licenseMd;
-}
+//   return licenseMd;
+// }
 
 
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  var badges = renderBadge(data.license);
+  var licenses = renderLicenses(data.license);
+  var badges = licenses.badge;
+  var licenseSection = licenses.section;
   var install = data.install.replace(/^/g, '\n- ');
   var usage = data.usage.replace(/^/g, '\n- ');
   var credits = data.credits.replace(/^/g, '\n- ');
@@ -92,8 +108,6 @@ ${install}
 ${usage}
 ${img}
 
-## License
-
 
 ## Contributing
 
@@ -109,9 +123,11 @@ ${credits}
 
 ## Contact
 
-For questions or learn how you may contribute to this project, please contact me:\n
+For questions or to learn how you may contribute to this project, please contact me:\n
 ### ${data.name}\n
 ### Project Link: https://github.com/${data.username}/${data.repo}
+
+${licenseSection}
 `
 }
 
